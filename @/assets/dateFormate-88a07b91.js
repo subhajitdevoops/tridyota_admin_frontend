b1daa9db2dfa626f59ad7e6ca1fbeaa228e3ae13
subhajitdevoops <121277474+subhajitdevoops@users.noreply.github.com>
2023-11-17 23:13:01 +0530

    Add files via upload

---
+++ @/assets/dateFormate-88a07b91.js
@@ -0,0 +1,1 @@
+import{d as o}from"./dayjs.min-61f3a08c.js";const m=(t,a)=>o(t).format(a),e=(t,a,r)=>o(t).format(`${a} ${r}`);export{e as a,m as s};
---
+++ @/assets/forgot-password-office-dark-8bc730dc.js
@@ -0,0 +1,1 @@
+const s="/@/assets/forgot-password-office-feb5e995.jpeg",e="/@/assets/forgot-password-office-feb5e995.jpeg";export{s as I,e as a};
